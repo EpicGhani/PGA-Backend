9867e1ba54986a64220f9ecc83a36896c5206bc3
Al-Ghani Abdulla <al.abdulla@strattonstudiogames.com>
2024-10-16 13:30:22 +0800

    [skip render] added item controller and navigation bar

---
+++ Item.API/Controllers/ItemsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Item.API.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Apparel()
+        {
+            return View();
+        }
+    }
+}
