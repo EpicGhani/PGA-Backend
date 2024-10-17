07a39b7385965f335d73571562e98aac791cf4a8
Al-Ghani Abdulla <al.abdulla@strattonstudiogames.com>
2024-10-17 11:35:44 +0800

    Added club parts data model

---
+++ Item.API/Models/Clubs/ShaftData.cs
@@ -0,0 +1,10 @@
+﻿namespace Item.API.Models.Clubs
+{
+    public class ShaftData
+    {
+        public string brandId { get; set; } = null!;
+        public int accuracy { get; set; } 
+        public int tempo { get; set; }
+        public int durability { get; set; }
+    }
+}
