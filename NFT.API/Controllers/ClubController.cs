6768b388f86fa59475b9a3b0fcb60e9ebfa7f884
Al-Ghani Abdulla <al.abdulla@strattonstudiogames.com>
2024-10-17 23:30:40 +0800

    Added extra payload validation for club crafting

--- NFT.API/Controllers/ClubController.cs
+++ NFT.API/Controllers/ClubController.cs
@@ -13,15 +13,28 @@ namespace NFT.API.Controllers
         public ClubController(NFTService NftService) => _service = NftService;
 
         [HttpPost("CraftClub")]
-        public async Task<ActionResult<ClubDataModel>> CraftClub(string headId, string shaftId, string gripId)
+        public async Task<IActionResult> CraftClub([FromBody]ClubPartsModel? clubParts, [FromQuery]string? headId, [FromQuery]string? shaftId, [FromQuery]string? gripId)
         {
-            var head = await _service.GetHeadDataByIdAsync(headId);
-            var shaft = await _service.GetShaftDataByIdAsync(shaftId);
-            var grip = await _service.GetGripDataByIdAsync(gripId);
+            if (headId == null && clubParts == null) return BadRequest(new { message = "Head ID is required" });
+            if (shaftId == null && clubParts == null) return BadRequest(new { message = "Shaft ID is required" });
+            if (gripId == null && clubParts == null) return BadRequest(new { message = "Grip ID is required" });
 
-            if (head is null) return NotFound(new {message = $"No head part found with ID: {headId}" });
-            if (shaft is null) return NotFound(new {message = $"No shaft part found with ID: {shaftId}" });
-            if (grip is null) return NotFound(new {message = $"No grip part found with ID: {gripId}" });
+            if (clubParts == null && (!string.IsNullOrWhiteSpace(headId) || !string.IsNullOrWhiteSpace(shaftId) || !string.IsNullOrWhiteSpace(gripId)))
+                clubParts = new ClubPartsModel
+                {
+                    HeadId = headId,
+                    ShaftId = shaftId,
+                    GripId = gripId
+                };
+
+
+            var head = await _service.GetHeadDataByIdAsync(clubParts.HeadId);
+            var shaft = await _service.GetShaftDataByIdAsync(clubParts.ShaftId);
+            var grip = await _service.GetGripDataByIdAsync(clubParts.GripId);
+
+            if (head is null) return NotFound(new {message = $"No head part found with ID: {clubParts.HeadId}" });
+            if (shaft is null) return NotFound(new {message = $"No shaft part found with ID: {clubParts.ShaftId}" });
+            if (grip is null) return NotFound(new {message = $"No grip part found with ID: {clubParts.GripId}" });
 
             if (!head.brandId.Equals(shaft.brandId) || !shaft.brandId.Equals(grip.brandId) || !grip.brandId.Equals(head.brandId))
                 return BadRequest(new { message = "Head, Shaft, and Grip must be of the same Brand" });
---
+++ NFT.API/Models/Clubs/ClubPartsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace NFT.API.Models.Clubs
+{
+    public class ClubPartsModel
+    {
+        public string HeadId { get; set; } = null!;
+        public string ShaftId { get; set; } = null!;
+        public string GripId { get; set; } = null!;
+    }
+}
