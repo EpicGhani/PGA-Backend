bcf99e8eefa3fc2be656b1247d30391201cc0927
Al-Ghani Abdulla <al.abdulla@strattonstudiogames.com>
2024-10-24 10:52:59 +0800

    removed wrong end points

--- Item.API/Services/ItemService.cs
+++ Item.API/Services/ItemService.cs
@@ -16,34 +16,6 @@ namespace Item.API.Services
             var mongoDatabase = mongoClient.GetDatabase(settings.Value.DatabaseName);
 
             // COLLECTION REFERENCE
-            _scalingBank = mongoDatabase.GetCollection<Scaling>(settings.Value.ScalingCollectionName);
         }
-
-        #region Club Related Services
-        // GET CLUB SCALING FOR DATA TABLE
-        public async Task<List<Scaling>> GetScalingsAsync() =>
-            await _scalingBank.Find(i => true).ToListAsync();
-
-        // GET SCALING BY TAG
-        public async Task<Scaling?> GetScalingByTagAsync(string tag)
-        {
-            var filter = Builders<Scaling>.Filter.Eq(i => i.scaleTag, tag.Trim().ToLower());
-
-            return await _scalingBank.Find(filter).FirstOrDefaultAsync();
-        }
-
-        public async Task<HeadData> GenerateHeadData()
-        {
-            return null;
-        }
-        public async Task<ShaftData> GenerateShaftData()
-        {
-            return null;
-        }
-        public async Task<GripData> GenerateGripData()
-        {
-            return null;
-        }
-        #endregion
     }
 }
