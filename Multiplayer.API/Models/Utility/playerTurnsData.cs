605f69e0516fd0a9f9ef45fb7573899b0816ce18
Al-Ghani Abdulla <al.abdulla@strattonstudiogames.com>
2024-10-25 13:55:13 +0800

    updated player turns data model

--- Multiplayer.API/Models/Utility/playerTurnsData.cs
+++ Multiplayer.API/Models/Utility/playerTurnsData.cs
@@ -3,6 +3,7 @@
     public class PlayerTurnsData
     {
         public int RngSeed { get; set; }
+        public string[] TakeThatsAppliedToSelf { get; set; } = null!;
         public string[] TakeThatsToApplyToOpponent { get; set; } = null!;
         public RecordedStrike RecordedStrike { get; set; } = null!;
     }
